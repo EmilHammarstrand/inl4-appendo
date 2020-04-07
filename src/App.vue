fbb0b38017f0c2986b99fa1a23c91a525b50d68c
Emil Hammarstrand <emilhammarstrand@hotmail.com>
2020-04-07 16:14:33 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    asd
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form />
+    <Home />
+    <List />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Form from "./components/Form"
+import Home from "./components/Home"
+import List from "./components/List"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Form,
+    Home,
+    List
   }
 }
 </script>
@@ -22,8 +26,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
