beb9bd91f9b18ae959f640c243e910560c82de2d
Fanny Ahlm <fannyahlm@fannys-mbp.mynet>
2020-04-07 16:46:17 +0200

    uppdaterat den lilla skillnanden mellan master på github och här

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,11 @@ import List from "./components/List"
 export default {
   name: 'App',
   components: {
-<<<<<<< HEAD
-    HelloWorld
+    Form,
+    Home,
+    List
   },
+  
   data: () => ({
     activityList: [
       {
@@ -40,15 +42,9 @@ export default {
 
     ]
 
-
-
   })
-=======
-    Form,
-    Home,
-    List
-  }
->>>>>>> fbb0b38017f0c2986b99fa1a23c91a525b50d68c
+
+
 }
 </script>
 
