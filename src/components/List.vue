41324d3a9fcb5f6c25c506b25699ebf40883b268
Karin Karlsson <karin.drake@gmail.com>
2020-04-08 15:02:41 +0200

    gjort klart sorteringsfunktioner, påbörjat css för list

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="root">
-        <button @click="sortByThis('activity')">Activity</button>
-        <button @click="sortByThis('category')">Category</button>
-        <button @click="sortByThis('tipster')">Tipster</button>
-        <button @click="sortByThis('estimatedTime')">Time</button>
-        <button @click="sortByThis('score')">Score</button>
+
+        <div class="sortButtons">
+            <button @click="sortByThis('activity')">Activity</button>
+            <button @click="sortByThis('category')">Category</button>
+            <button @click="sortByThis('tipster')">Tipster</button>
+            <button @click="sortByThis('estimatedTime')">Time</button>
+            <button @click="sortByThis('score')">Score</button>
+        </div>
+        
         <div v-for="activity in sortList" class="activityCard" :key="activity.activity">
             <h3>{{activity.activity}}</h3> 
             <p> <span>Category:</span>  {{activity.category}}</p>
@@ -53,8 +57,48 @@ export default {
                 })
                 return copy
             }
+            if(this.sortBy == "tipster"){
+                copy.sort( (a,b) => {
+                    if(a.tipster.toLowerCase() < b.tipster.toLowerCase()){
+                        return -1;
+                    } else if(a.tipster.toLowerCase() > b.tipster.toLowerCase()){
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                })
+                return copy
+            }
+            if(this.sortBy == "estimatedTime"){
+                copy.sort( (a,b) => {
+                if(a.estimatedTime.toLowerCase() < b.estimatedTime.toLowerCase()){
+                    return -1;
+                } else if(a.estimatedTime.toLowerCase() > b.estimatedTime.toLowerCase()){
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            return copy
+            }
+            if(this.sortBy == "score"){
+                copy.sort( (a,b) => {
+                if(a.score.toLowerCase() < b.score.toLowerCase()){
+                    return -1;
+                } else if(a.score.toLowerCase() > b.score.toLowerCase()){
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            return copy
+            }
 
             return copy
+            
+            
+
+           
         },
         
     },//slut computed
@@ -73,17 +117,37 @@ export default {
 </script>
 
 <style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
+    *{
+        font-family:'Quicksand', sans-serif;
+    }
     div.activityCard{
-        border: 2px solid grey;
+        border-bottom: 2px solid grey;
         margin-bottom: .5rem;
+        background-color:#465F6F;
+        color:white;
+   
 
     }
     span{
         font-weight: 600;
     }
-    p,
     h3{
-        margin-left: .5rem;
+        padding-top:0.5em;
+        padding-bottom:0.3em;
+        margin: .3rem .3rem .3rem .5rem
+
+    }
+    p{
+       margin: .3rem .3rem .3rem .5rem;
+       
+    }
+    .sortButtons > button{
+        background-color:#96BB53;
+        padding:0.5em;
+        border:1px solid white;
+        border-radius:0.5em;
+       
     }
 
 
