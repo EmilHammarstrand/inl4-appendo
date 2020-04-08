7a49340a9bb01f5a5e468869ccb8a5499d762031
Emil Hammarstrand <emilhammarstrand@hotmail.com>
2020-04-08 12:09:00 +0200

    Validated category and estimated time

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,33 +1,35 @@
 <template>
     <div class="root">
         <div class="form-group">
-            <label for="name">Name: </label>
+            <label for="name">Tipser: </label> <br />
             <input type="text" name="name" placeholder="Enter your name" v-model="name" :class="nameClass" @blur.once="nameIsTouched = true" >
             <span v-if="nameIsTouched && !nameIsValid" class="error"> {{ nameErrorMessage }} </span>
         </div>
         <div class="form-group">
-            <label for="activity">Activity suggestion: </label>
+            <label for="activity">Activity: </label> <br />
             <input type="text" placeholder="Enter a short suggestion" name="activity" v-model="activity" :class="activityClass" @blur.once="activityIsTouched = true" >
             <span v-if="!activityIsValid && activityIsTouched" class="error"> {{ activityErrorMessage }} </span>
         </div>
         <div class="form-group">
-            <label for="dropdown">Category: </label>
-             <select name="dropdown" v-model="selected">
+            <label for="dropdown">Category: </label> <br />
+             <select name="dropdown" v-model="selected" :class="categoryClass" @blur.once="categoryIsTouched = true">
                 <option>Outside</option>
                 <option>Food</option>
                 <option>Training</option>
                 <option>Thinking</option>
              </select>
+             <span v-if="categoryIsValid && categoryIsTouched" class="error"> {{ categoryErrorMessage }} </span>
         </div>
         <div class="form-group">
-            <label for="estimatedTime">Estimated time: </label>
-            <input name="estimatedTime" type="number" v-model="estimatedTime" min="3" max="15" >
+            <label for="estimatedTime">Estimated minutes: </label> <br />
+            <input class="estTime" name="estimatedTime" type="number" v-model="estimatedTime" min="3" max="15" :class="timeClass" @blur.once="timeIsTouched = true" >
+            <span v-if="!timeIsValid && timeIsTouched" class="error"> {{ timeErrorMessage }} </span>
         </div>
         <div class="form-group">
             <button :disabled="!isCompleted || !nameIsValid || !activityIsValid" @click="formResult = true;"> Post</button>
         </div>
          
-        <span v-if="formResult"> Hello {{name}}, you choose {{activity}} in category {{selected}}, estimated time {{estimatedTime}} minutes -{{todaysDate}}- </span>
+        <span v-if="formResult"> Hello {{name}}, you choose {{activity}} in category {{selected}}, estimated time {{estimatedTime}} minutes, published: {{todaysDate}} </span>
     </div>
 </template>
 
@@ -41,6 +43,8 @@ export default {
         date: "",
         nameIsTouched: false,
         activityIsTouched: false,
+        categoryIsTouched: false,
+        timeIsTouched: false,
         formResult: false
     }),
 
@@ -55,6 +59,13 @@ export default {
 			if( !this.nameIsTouched ) return '';
 			return this.nameIsValid ? 'valid' : 'invalid';
         },
+        categoryIsValid() {
+            return this.selected == 0;
+        },
+        categoryClass() {
+			if( !this.categoryIsTouched ) return '';
+			return this.categoryIsValid ? 'valid' : 'invalid';
+        },
         activityErrorMessage(){
             return "Must be between 2-10 characters"
         },
@@ -62,7 +73,6 @@ export default {
             let asd = this.activity.length <= 10 && this.activity.length >= 2;
             return asd;
         },
-
         activityClass() {
 			if( !this.activityIsTouched ) return '';
 			return this.activityIsValid ? 'valid' : 'invalid';
@@ -73,14 +83,27 @@ export default {
         todaysDate(){
          let todaysDate = new Date().toLocaleString();
          return todaysDate;
-        }
+        },
+        categoryErrorMessage(){
+            return "You must choose a category";
+        },
+        timeErrorMessage(){
+            return "Must be between 3-15 minutes";
+        },
+        timeIsValid() {
+            let qwe = this.estimatedTime >=3 && this.estimatedTime <= 15;
+            return qwe;
+        },
+        timeClass() {
+			if( !this.timeIsTouched ) return '';
+			return this.timeIsValid ? 'valid' : 'invalid';
+        },
     },
 
     methods: {
         postActivityBtn(){
             
         }
-        
     }
 
 
@@ -95,5 +118,19 @@ export default {
 
     textarea.valid { border-color: green; }
     textarea.invalid { border-color: red; }
+
+    select.valid { border-color: red; }
+    select.invalid { border-color: green; }
+
+    .error{
+        color: red;
+    }
+    .form-group{
+        margin: 1em;
+
+    }
+    .estTime{
+        width: 2.3em;
+    }
 </style>
 
